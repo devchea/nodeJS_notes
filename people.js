6bf025a6bc6dbf533a11af45881dbb889e3498c5
devchea <rchea817@gmail.com>
2021-01-03 20:38:32 -0800

    "updates"

--- people.js
+++ people.js
@@ -11,4 +11,4 @@ module.exports = {
     #         ["b", 2,  "blue" ],
     #         ["c", 3,  "yellow" ],
     #         ["d", 4,  "green" ], 
-    #         ["e", 5, "orange"]
+    #         ["e", 5, "orange"
